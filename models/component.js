a9b776849260ea163567e233baf37b6bc23db065
Nader Hamekasi <nhamekasi@RWCORP>
2018-02-12 11:06:05 -0700

    moving component type logic to component

--- models/component.js
+++ models/component.js
@@ -27,3 +27,46 @@ Component.prototype.clone = function () {
     return result;
 }
 
+
+Component.prototype.overridesNumberSets = function () {
+    return this.type != "";
+}
+
+scopComponent.prototype.overridesNumberReps = function () {
+    var compType = this.type;
+    return compType != "Tabata" && compType != "AMRAP";
+}
+
+Component.prototype.overridesRest = function () {
+    var compType = this.type;
+    return compType != "Tabata" && compType != "AMRAP" && compType != "EMOM";
+}
+
+
+Component.prototype.showComponentRounds = function (component) {
+    return component.type == 'Superset' || component.type == 'Tabata' || component.type == 'EMOM';
+}
+
+Component.prototype.showComponentRest = function (component) {
+    return component.type == 'Superset' || component.type == 'Tabata';
+}
+
+Component.prototype.showComponentDuration = function (component) {
+    return component.type == 'AMRAP' || component.type == 'EMOM' || component.type == 'Tabata';
+}
+
+Component.prototype.getDurationLabel = function (component) {
+    if (component.type == 'AMRAP') return 'in';
+    if (component.type == 'EMOM') return 'every';
+    if (component.type == 'Tabata') return 'activity';
+}
+
+Component.prototype.getRestLabel = function (component) {
+    if (component.type == 'Tabata') return 'rest'
+    return 'rest';
+}
+
+Component.prototype.needsManualStart = function () {
+    var componentType = this.type;
+    return componentType == "AMRAP" || componentType == "EMOM" || componentType == "Tabata"
+}
